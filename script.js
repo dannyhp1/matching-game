1c83cd6dc9a41738ce580608fbfd236ac5bd1634
Danny Pham <dannyhp1@uci.edu>
2018-08-16 13:12:13 -0700

    Added matching logic, still need to reflip cards if not matching

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
 // Get all cards on the board.
 const allCards = document.querySelectorAll(".matching-card");
 
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
 function flipCard() {
 	// 'this' refers to the the element that caused the event.
-	this.classList.toggle("flip");
+	this.classList.add("flip");
+
+	if(!hasFlippedCard) {
+		/* First time the player flipped the card. */
+		hasFlippedCard = true;
+		firstCard = this;
+
+		console.log([hasFlippedCard, firstCard]);
+	} else {
+		/* Second time the player flipped the card. */
+		hasFlippedCard = false;
+		secondCard = this;
+
+		/* Check to see if the cards matched. */
+		if(firstCard.dataset.framework === secondCard.dataset.framework) {
+			firstCard.removeEventListener("click", flipCard);
+			secondCard.removeEventListener("click", flipCard);
+		}
+	}
 }
 
 // For each card, whenever it's clicked, execute the function: flipCard()
